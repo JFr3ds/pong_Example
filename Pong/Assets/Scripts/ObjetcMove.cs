c096dbcfbd54b80f9956af2a8e68f39366654989
Freds <jfrelancer@gmail.com>
2022-03-23 21:07:51 -0500

    Limites de pantalla
    Demarcacion del area de juego

--- Pong/Assets/Scripts/ObjetcMove.cs
+++ Pong/Assets/Scripts/ObjetcMove.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class ObjetcMove : MonoBehaviour
 {
-    [SerializeField] private string _msj;
-    
-    void Update()
+    [SerializeField] private Camera m_mainCamera;
+    [SerializeField] private float m_disFromCamera;
+    [SerializeField] private float m_sphereRadius;
+    [SerializeField] private GameObject m_ball;
+    [SerializeField] private float m_speedBall;
+    private Vector2 m_ballDirection;
+
+    private void OnDrawGizmos()
     {
-        if (Input.GetButtonDown("Jump"))
+        Vector3 screenReference = new Vector3(Screen.width, Screen.height, m_disFromCamera);
+        Vector3[] desirePositions = {
+            new Vector3(0,0,m_disFromCamera),
+            new Vector3(0,screenReference.y, m_disFromCamera),
+            screenReference,
+            new Vector3(screenReference.x, 0, m_disFromCamera)
+        };
+
+        for (int i = 0; i < desirePositions.Length; i++)
         {
-            Debug.Log(_msj);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(m_mainCamera.ScreenToWorldPoint(desirePositions[i]), m_sphereRadius);
+            Gizmos.color = Color.green;
+            if (i > 0)
+            {
+                Gizmos.DrawLine(m_mainCamera.ScreenToWorldPoint (desirePositions[i]), m_mainCamera.ScreenToWorldPoint(desirePositions[i - 1]));
+            }
+            else
+            {
+                Gizmos.DrawLine(m_mainCamera.ScreenToWorldPoint(desirePositions[i]), m_mainCamera.ScreenToWorldPoint(desirePositions[desirePositions.Length-1]));
+            }
         }
     }
 }
